e3dce78294125d24056f1032bb43208f45b6485c
Lautaro Fullone <lautarofullone@gmail.com>
2024-09-04 00:47:23 -0300

    init project: Layout first check

---
+++ components/layout/FollowBar.tsx
@@ -0,0 +1,10 @@
+export const FollowBar = () => {
+   return (
+      <div className="hidden px-6 pt-4 lg:block">
+         <div className="bg-neutral-800 rounded-xl p-4">
+            <h2 className="text-white text-xl font-semibold"> Who To Follow</h2>
+            <div className="flex flex-col gap-6 mt-4"></div>
+         </div>
+      </div>
+   )
+}
